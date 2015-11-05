6795850eb3b8a80a95420a08363440e8593c4fb4
Donghwa Lee <dlee@DHL-MPB13.local>
2015-11-05 05:54:50 -0500

    log in, session methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < ActionController::Base
     render '/welcome'
   end
 
+  def amiloggedin
+    amiloggedin = !!session[:current_user_id]
+    render json: current_user
+  end
+
+  private
+
+  def current_user
+
+    if session[:current_user_id]
+      @current_user ||= User.find(session[:current_user_id])
+    else
+      @current_user = nil
+    end
+
+  end
+
 end
