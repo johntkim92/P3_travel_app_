5301117a518326ef6611e0605ba46176431b96f6
Donghwa Lee <dlee@DHL-MPB13.local>
2015-11-05 05:38:37 -0500

    welcome html file and method in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users
+
   # setting root
   root 'application#welcome'
+
+
+  # users - did not do only yet to limit other RESTful actions - as we may need others to update, delete, etc.
+  resources :users
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/migrate/20151105101618_create_users.rb
+++ db/migrate/20151105101618_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password
-      t.string :username
-      t.string :full_name
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :username, null: false
+      t.string :full_name, null: false
 
       t.timestamps null: false
     end
+
+    add_index :users, :username
   end
 end
