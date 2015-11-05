392856e06225844588a5340aa87ed07b7d57f0d5
Donghwa Lee <dlee@DHL-MPB13.local>
2015-11-05 05:46:14 -0500

    updated users model and create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
+
+  # added allow_nil to others than password as they might be helpful when updating others???
+
+  validates :username, presence: true, uniqueness: true, allow_nil: true
+
+  validates :password_digest, presence: true
+  validates :password, length: {
+    minimum: 8, allow_nil: true
+  }
+
+  validates :email, presence: true, uniqueness: true, allow_nil:true
+
+  validates :full_name, presence: true, allow_nil: true
+
 end
