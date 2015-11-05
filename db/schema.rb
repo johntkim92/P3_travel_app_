e5dac2656dffd33e2e900a00c53571de347452f8
Donghwa Lee <dlee@DHL-MPB13.local>
2015-11-05 07:55:26 -0500

    trips resource created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105101618) do
+ActiveRecord::Schema.define(version: 20151105124747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title",       null: false
+    t.string   "destination", null: false
+    t.string   "description", null: false
+    t.string   "start_date",  null: false
+    t.string   "end_date",    null: false
+    t.string   "tags",        null: false
+    t.string   "notes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "trips", ["tags"], name: "index_trips_on_tags", using: :btree
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -27,4 +43,5 @@ ActiveRecord::Schema.define(version: 20151105101618) do
 
   add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
+  add_foreign_key "trips", "users"
 end
