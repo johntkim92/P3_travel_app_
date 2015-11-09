dc5bb77293f76b63c0bc11f1006f98781c3a2e53
afreem66 <afreem66@gmail.com>
2015-11-09 15:59:48 -0500

    added new marker functionality to search

--- app/assets/javascripts/ng_map.js
+++ app/assets/javascripts/ng_map.js
@@ -5,9 +5,12 @@ myApp.controller('mapController', function ($scope) {
   $scope.placeChanged = function () {
     $scope.place = this.getPlace();
     var dest = $scope.place.geometry.location
-    $scope.map.panTo({lat: dest.lat(), lng: dest.lng()})
-    setZoom(zoom: 12)
-  }
-
-
+    new google.maps.Marker({
+        map: $scope.map,
+        position: dest
+      })
+      locations.push({lat: dest.lat(), lng: dest.lng()})
+      $scope.map.panTo({lat: dest.lat(), lng: dest.lng()})
+      // $scope.map.setZoom(7)
+    }
 })
