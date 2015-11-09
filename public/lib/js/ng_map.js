12572464c9fc64edf372a7004023758247207f89
Donghwa Lee <donghwalee328@gmail.com>
2015-11-09 14:41:23 -0500

    test

---
+++ public/lib/js/ng_map.js
@@ -0,0 +1,11 @@
+alert("jeu");
+
+var myApp = angular.module("mapp", ['ngMap'])
+
+myApp.controller('mapController', function ($scope){
+  $scope.placeChanged = function () {
+    $scope.place = this.getPlace();
+    var location = $scope.place.geometry.location
+      console.log(location.lat(), location.lng());
+  }
+})
