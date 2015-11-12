542b962a9b56a250a214488d6323fa363f178994
Donghwa Lee <donghwalee328@gmail.com>
2015-11-12 10:38:23 -0500

    added user_marker field to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
 
+  USERMARKER = [
+    "/lib/images/marker_andrew.png",
+    "/lib/images/marker_john.png",
+    "/lib/images/marker_tendai.png",
+    "/lib/images/marker_donghwa.png"
+  ]
+
   # added allow_nil to others than password as they might be helpful when updating others???
 
   validates :username, presence: true, uniqueness: true, allow_nil: true
